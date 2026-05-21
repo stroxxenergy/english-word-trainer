9edec26e3fc3bbe4de7997ce298e389572972633
stroxxenergy <stroxxenergy@gmail.com>
2026-05-21 13:44:29 +0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQlz_FhXdpkejlOwiz0CKeLs62B-uj2Zo",
+  authDomain: "english-word-trainer-22c75.firebaseapp.com",
+  projectId: "english-word-trainer-22c75",
+  storageBucket: "english-word-trainer-22c75.firebasestorage.app",
+  messagingSenderId: "444579658966",
+  appId: "1:444579658966:web:a5d2314775f45c9c925d77"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
